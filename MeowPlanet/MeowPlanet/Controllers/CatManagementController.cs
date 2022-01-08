0b90ea93f42b92e49435fbfb27a7fa3afb402831
Eishin Chen <applejack9972@gmaiil.com>
2022-01-08 22:56:30 +0800

    新增貓咪上傳功能

--- MeowPlanet/MeowPlanet/Controllers/CatManagementController.cs
+++ MeowPlanet/MeowPlanet/Controllers/CatManagementController.cs
@@ -1,10 +1,13 @@
 ﻿using MeowPlanet.Models;
+using MeowPlanet.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MeowPlanet.Controllers
@@ -12,24 +15,33 @@ namespace MeowPlanet.Controllers
     public class CatManagementController : Controller
     {
         private readonly Models.MeowContext _dbcontext;
-        public CatManagementController(Models.MeowContext dbcontext)
+        private readonly IWebHostEnvironment _env;
+        public CatManagementController(Models.MeowContext dbcontext, IWebHostEnvironment env)
         {
             this._dbcontext = dbcontext;
+            this._env = env;
         }
         //顯示沒有貓咪的畫面
-        public IActionResult AddCat()
+        public async Task<IActionResult> AddCat()
         {
+            ClaimsPrincipal claims = HttpContext.User;
+            int userId = Convert.ToInt32(claims.Identity.Name);
+            var info = await(from a in _dbcontext.Cats
+                             where a.UserId == userId
+                             select a).FirstOrDefaultAsync();
+            var ROLE = claims.Claims.Where(x => x.Type == ClaimTypes.Role).FirstOrDefault().Value;
             return View();
         }
         //顯示上傳貓咪的畫面
         public IActionResult UploadCat()
         {
+            TempData["Photo1"] = "/images/我是誰(小問號).PNG";
             return View();
         }
+
+        //上傳貓咪資訊
         [HttpPost]
-        [Route("/CatManagement/uploadconfirm")]
-        [Consumes("application/x-www-form-urlencoded")]
-        public async Task<MemberManagement.Message> UploadConfirm(IFormCollection info)
+        public async Task<MemberManagement.Message> UploadConfirm(IFormFileCollection files, CatViewModel model)
         {
             var claims = HttpContext.User;
             var ID = Convert.ToInt32(claims.Identity.Name);
@@ -52,47 +64,78 @@ namespace MeowPlanet.Controllers
                 }
                 else
                 {
-                    try
-                    {
-                        var cat = new Cat
-                        {
-                            UserId = ID,
-                            Image = "/images/預設大頭貼.png",
-                            Name = info["Name"],
-                            Country = info["Country"],
-                            City = info["City"],
-                            CatColor = info["CatColor"],
-                            CatGender = Convert.ToInt32(info["CatGender"]),
-                            Age = info["Age"],
-                            Ligation = info["Ligation"],
-                            Vaccine = info["Vaccine"],
-                            Chip = info["Chip"],
-                            Remark = info["Remark"]
-                        };
-                        _dbcontext.Cats.Add(cat);
-                        _dbcontext.SaveChanges();
-                        Console.WriteLine("貓咪資訊新增成功");
+                    String root = _env.ContentRootPath + @"\wwwroot\images\catManagement";
 
-                        msg = new MemberManagement.Message()
-                        {
-                            Code = 200,
-                            Msg = $"連線成功! 貓咪資訊新增成功",
-                            Time = DateTime.Now
-                        };
-                    }
-                    catch (DbUpdateException ex)
+                    model.Image = new List<String>();
+                    var sum = files.Sum(f => f.Length);
+                    if (files == null || sum == 0)
                     {
-                        Console.WriteLine("錯誤:" + ex);
                         msg = new MemberManagement.Message()
                         {
                             Code = 400,
-                            Msg = $"資料新增失敗" + ex,
+                            Msg = $"請上傳圖片",
                             Time = DateTime.Now
                         };
                     }
+                    else
+                    {
+                        foreach (IFormFile file in files)
+                        {
+                            string fileName = file.FileName;
+                            using (var stream = System.IO.File.Create(root + fileName))
+                            {
+                                await file.CopyToAsync(stream);
+                                var path = root + fileName;
+                                model.Image.Add(fileName);
+                            }
+                        }
+                        //List<String> to string
+                        var result = String.Join(", ", model.Image.ToArray());
+                        try
+                        {
+                            var cat = new Cat
+                            {
+                                UserId = ID,
+                                Image = result,
+                                Name = model.Name,
+                                Country = model.Country,
+                                City = model.City,
+                                CatColor = model.CatColor,
+                                CatGender = Convert.ToInt32(model.CatGender),
+                                Age = model.Age,
+                                Ligation = model.Ligation,
+                                Vaccine = model.Vaccine,
+                                Chip = model.Chip,
+                                Remark = model.Remark
+                            };
+                            _dbcontext.Cats.Add(cat);
+                            _dbcontext.SaveChanges();
+                            Console.WriteLine("貓咪資訊新增成功");
+
+                            msg = new MemberManagement.Message()
+                            {
+                                Code = 200,
+                                Msg = $"連線成功! 貓咪{model.Name}新增成功",
+                                Time = DateTime.Now
+                            };
+                        }
+                        catch (DbUpdateException ex)
+                        {
+                            Console.WriteLine("錯誤:" + ex);
+                            msg = new MemberManagement.Message()
+                            {
+                                Code = 400,
+                                Msg = $"資料新增失敗" + ex,
+                                Time = DateTime.Now
+                            };
+                        }
+                    }
+                    
                 } 
             }
             return msg;
         }
+    
+        
     }
 }
