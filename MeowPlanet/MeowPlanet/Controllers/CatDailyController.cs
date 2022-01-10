24d8b36a2a6f22b12832d923d435a14b1c348586
LAPTOP-6IR4E4AF\Tibame_T14 <CatDaily>
2022-01-10 11:04:30 +0800

    留言板網頁製作第一次上傳

---
+++ MeowPlanet/MeowPlanet/Controllers/CatDailyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeowPlanet.Controllers
+{
+    public class CatDailyController : Controller
+    {
+        public IActionResult CatDaily1()
+        {
+            return View();
+        }
+        public IActionResult CatDaily2()
+        {
+            return View();
+        }
+        public IActionResult CatDaily3()
+        {
+            return View();
+        }
+        public IActionResult CatDaily4()
+        {
+            return View();
+        }
+    }
+}
