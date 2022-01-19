1249f9ca1f63d705a483afc0c18383882678575e
LAPTOP-6IR4E4AF\Tibame_T14 <waterlv7owork@gmail.com>
2022-01-19 15:06:27 +0800

    注入程式碼

--- MeowPlanet/MeowPlanet/Models/CollectionListJoinCat.cs
+++ MeowPlanet/MeowPlanet/Models/CollectionListJoinCat.cs
@@ -5,7 +5,18 @@ using System.Threading.Tasks;
 
 namespace MeowPlanet.Models
 {
-    public class CollectionListJoinCat
+    public partial class CollectionListJoinCat
     {
+        //public int CollectionListId { get; set; }
+        public int UserId { get; set; }
+        public int CatId { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int? IsDeleted { get; set; }
+
+        //public virtual Cat Cat { get; set; }
+        //public virtual UserData User { get; set; }
     }
 }
