95ba39480695a465ccbbeb54d8a70aba79284920
Yao-0x68 <mf99008@gmail.com>
2022-01-21 14:00:48 +0800

    Revert "Merge branch 'master' of https://github.com/EishinCHEN/MeowPlanet"
    This reverts commit 7028e1fcbe552a676204196b90679d632f90b75e, reversing
    changes made to 70bdc98ee2e96639ab9f8aa2a0ac354178c13fa3.

--- MeowPlanet/MeowPlanet/Controllers/CatPlanetInhabitantsController.cs
+++ MeowPlanet/MeowPlanet/Controllers/CatPlanetInhabitantsController.cs
@@ -147,7 +147,7 @@ namespace MeowPlanet.Controllers
             {
                 _dbContext.CollectionLists.Remove((CollectionList)deletelist);
                 _dbContext.SaveChanges();
-                msg = "已取消收藏";
+                msg = "成功刪除";
             }
 
             return msg;
