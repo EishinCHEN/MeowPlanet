b091c8388536bafe49d8ac08534969b652133fa4
LAPTOP-6IR4E4AF\Tibame_T14 <waterlv7owork@gmail.com>
2022-01-24 10:52:31 +0800

    整理Cat控制器內部code

--- MeowPlanet/MeowPlanet/Controllers/CatCollectController.cs
+++ MeowPlanet/MeowPlanet/Controllers/CatCollectController.cs
@@ -29,23 +29,18 @@ namespace MeowPlanet.Controllers
             this._webHostEnvironment = _hostEnvironment;//不知道功能 先省略
         }
 
-        /*[Authorize]  */        //限定已登入的使用者才能瀏覽
+        
         //預先傳出登入者資訊到前端
         public async Task<IActionResult> CatCollect()
         {
             var claims = HttpContext.User;
             var ID = Convert.ToInt32(claims.Identity.Name);
 
-            var info = await (from a in _dbcontext.UserDatas
-                              where a.UserId == ID
-                              select a).FirstOrDefaultAsync();
-            //var info = ID;
+            //var info = await (from a in _dbcontext.UserDatas
+            //                  where a.UserId == ID
+            //                  select a).FirstOrDefaultAsync();
 
             ViewData["UserID"] = ID;
-            //ViewData["UserID"] = "123";
-            var b = ViewData["UserID"];
-            //var ROLE = claims.Claims.Where(x => x.Type == ClaimTypes.Role).FirstOrDefault().Value;
-            //TempData["Role"] = ROLE;
             return View();
         }
 
@@ -55,10 +50,8 @@ namespace MeowPlanet.Controllers
         [ProducesAttribute("application/json")]
         [Consumes("application/x-www-form-urlencoded")]
         public List<Models.CollectionListJoinCat> Catjson(IFormCollection search)
-        //public ActionResult Catjson(IFormCollection search)
         {
             Console.WriteLine("Account:" + search["User_ID"]);
-            //var join = _dbcontext.Cats.AsQueryable();
             var join = (IQueryable<CollectionListJoinCat>)(from j in _dbcontext.CollectionLists
                                                            join k in _dbcontext.Cats
                                                            on j.CatId equals k.CatId
